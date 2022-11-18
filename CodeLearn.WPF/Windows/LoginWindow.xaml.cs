008d3391d8156c10850872161960d70447c9ebc2
ZubriQ <zubrich157@gmail.com>
2022-11-18 19:33:04 +0700

    Added LoginWindow, Student/HomeWindow, a codelearn logo. Changed some theme styles.

---
+++ CodeLearn.WPF/Windows/LoginWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CodeLearn.WPF.Windows
+{
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Login_Click(object sender, RoutedEventArgs e)
+        {
+            //StudentWindow window = new();
+            //window.Show();
+            Close();
+        }
+    }
+}
