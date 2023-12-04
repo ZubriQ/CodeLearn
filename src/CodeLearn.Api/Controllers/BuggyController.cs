d4062cdd25df081653e1b00815fee72c96338b4e
ZubriQ <zubrich157@gmail.com>
2023-12-04 22:37:25 +0700

    feat: buggy controller

---
+++ src/CodeLearn.Api/Controllers/BuggyController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CodeLearn.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public sealed class BuggyController : ControllerBase
+{
+    [HttpGet("not-found")]
+    public ActionResult GetNotFound()
+    {
+        return NotFound();
+    }
+
+    [HttpGet("bad-request")]
+    public ActionResult GetBadRequest()
+    {
+        return BadRequest(new ProblemDetails { Title = "This is a Bad Request" });
+    }
+
+    [HttpGet("unauthorised")]
+    public ActionResult GetUnauthorised()
+    {
+        return Unauthorized();
+    }
+
+    [HttpGet("validation-error")]
+    public ActionResult GetValidationError()
+    {
+        ModelState.AddModelError("Problem1", "This is the first Error");
+        ModelState.AddModelError("Problem2", "This is the second Error");
+        return ValidationProblem();
+    }
+
+    [HttpGet("server-error")]
+    public ActionResult GetServerError()
+    {
+        throw new Exception("This is a server Error");
+    }
+}
