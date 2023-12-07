1f41d4f730ee8d14a01095f8a2b695b178b76d1e
ZubriQ <zubrich157@gmail.com>
2023-12-08 00:39:35 +0700

    feat: difficulty constants

---
+++ src/CodeLearn.Domain/Constants/Difficulties.cs
@@ -0,0 +1,8 @@
+﻿namespace CodeLearn.Domain.Constants;
+
+public abstract class Difficulties
+{
+    public const string Easy = nameof(Easy);
+    public const string Medium = nameof(Medium);
+    public const string Hard = nameof(Hard);
+}
