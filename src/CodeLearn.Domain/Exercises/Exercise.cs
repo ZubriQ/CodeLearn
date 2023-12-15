1a1d8da5ce770be572eb5a8904ba0e35d0976d00
ZubriQ <zubrich157@gmail.com>
2023-12-16 01:17:02 +0700

    feat: add note method

--- src/CodeLearn.Domain/Exercises/Exercise.cs
+++ src/CodeLearn.Domain/Exercises/Exercise.cs
@@ -2,12 +2,18 @@
 
 namespace CodeLearn.Domain.Exercises;
 
-public abstract class Exercise : BaseEntity<ExerciseId>, IAggregateRoot
+public abstract class Exercise(
+    ExerciseId id,
+    TestingId testingId,
+    string title,
+    string description,
+    ExerciseDifficulty difficulty)
+    : BaseEntity<ExerciseId>(id), IAggregateRoot
 {
-    public TestingId TestingId { get; private set; }
-    public string Title { get; private set; }
-    public string Description { get; private set; }
-    public ExerciseDifficulty Difficulty { get; private set; }
+    public TestingId TestingId { get; private set; } = testingId;
+    public string Title { get; private set; } = title;
+    public string Description { get; private set; } = description;
+    public ExerciseDifficulty Difficulty { get; private set; } = difficulty;
 
     private readonly IList<ExerciseNote> _exerciseNotes = [];
     public IReadOnlyList<ExerciseNote> ExerciseNotes => _exerciseNotes.AsReadOnly();
@@ -15,23 +21,14 @@ public abstract class Exercise : BaseEntity<ExerciseId>, IAggregateRoot
     private readonly List<ExerciseTopic> _exerciseTopics = [];
     public IEnumerable<ExerciseTopic> ExerciseTopics => _exerciseTopics.AsReadOnly();
 
-    protected Exercise(
-        ExerciseId id,
-        TestingId testingId,
-        string title,
-        string description,
-        ExerciseDifficulty difficulty)
-        : base(id)
-    {
-        TestingId = testingId;
-        Title = title;
-        Description = description;
-        Difficulty = difficulty;
-    }
-
-    public void AddExerciseTopic(ExerciseTopic exerciseTopic)
+    public void AddTopic(ExerciseTopic exerciseTopic)
     {
         _exerciseTopics.Add(exerciseTopic);
         exerciseTopic.AddExercise(this);
+    }
+
+    public void AddNote(ExerciseNote exerciseNote)
+    {
+        _exerciseNotes.Add(exerciseNote);
     }
 }
