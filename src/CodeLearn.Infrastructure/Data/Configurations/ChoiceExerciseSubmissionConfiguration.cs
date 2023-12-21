316f71be093cb2686a467d69ce6966f9d34bafce
ZubriQ <zubrich157@gmail.com>
2023-12-21 15:35:29 +0700

    refactor: add configuration method

--- src/CodeLearn.Infrastructure/Data/Configurations/ChoiceExerciseSubmissionConfiguration.cs
+++ src/CodeLearn.Infrastructure/Data/Configurations/ChoiceExerciseSubmissionConfiguration.cs
@@ -10,20 +10,25 @@ public sealed class ChoiceExerciseSubmissionConfiguration : IEntityTypeConfigura
 {
     public void Configure(EntityTypeBuilder<ChoiceExerciseSubmission> builder)
     {
+        ConfigureChoiceExerciseSubmission(builder);
+    }
+
+    private static void ConfigureChoiceExerciseSubmission(EntityTypeBuilder<ChoiceExerciseSubmission> builder)
+    {
         builder
-          .HasMany(e => e.Choices)
-          .WithMany(e => e.ExerciseSubmissions)
-          .UsingEntity<SelectedChoice>(
-              nameof(SelectedChoice),
-              j => j
-                  .HasOne<QuestionChoice>()
-                  .WithMany()
-                  .HasForeignKey(nameof(QuestionChoiceId))
-                  .OnDelete(DeleteBehavior.NoAction),
-              j => j
-                  .HasOne<ChoiceExerciseSubmission>()
-                  .WithMany()
-                  .HasForeignKey(nameof(ExerciseSubmissionId))
-                  .HasPrincipalKey("Id"));
+            .HasMany(e => e.Choices)
+            .WithMany(e => e.ExerciseSubmissions)
+            .UsingEntity<SelectedChoice>(
+                nameof(SelectedChoice),
+                j => j
+                    .HasOne<QuestionChoice>()
+                    .WithMany()
+                    .HasForeignKey(nameof(QuestionChoiceId))
+                    .OnDelete(DeleteBehavior.NoAction),
+                j => j
+                    .HasOne<ChoiceExerciseSubmission>()
+                    .WithMany()
+                    .HasForeignKey(nameof(ExerciseSubmissionId))
+                    .HasPrincipalKey("Id"));
     }
 }
