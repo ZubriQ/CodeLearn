5f2ddcdb12508cecadc08f137e996a5491ce1503
ZubriQ <zubrich157@gmail.com>
2024-01-02 05:42:10 +0700

    feat: header, error, not-found components

--- src/CodeLearn.WebApp/src/components/header/index.tsx
+++ src/CodeLearn.WebApp/src/components/header/index.tsx
@@ -1,3 +1,90 @@
+import { useState } from 'react';
+import { Dialog, Popover } from '@headlessui/react';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
+import { Link } from 'react-router-dom';
+
 export default function Header() {
-  return <div>Header</div>;
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  return (
+    <header className="bg-white">
+      <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 sm:px-6" aria-label="Global">
+        <div className="flex sm:flex-1">
+          <a href="/" className="-m-1.5 p-1.5">
+            <span className="sr-only text-amber-500">CodeLearn</span>
+            <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?" alt="" />
+          </a>
+        </div>
+        <div className="flex sm:hidden">
+          <button
+            type="button"
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+            onClick={() => setMobileMenuOpen(true)}
+          >
+            <span className="sr-only">Open main menu</span>
+            <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+          </button>
+        </div>
+        <Popover.Group className="hidden sm:flex sm:gap-x-12">
+          <Link to="testings" className="text-sm font-semibold leading-6 text-gray-900">
+            Testings
+          </Link>
+          <Link to="about" className="text-sm font-semibold leading-6 text-gray-900">
+            About
+          </Link>
+        </Popover.Group>
+        <div className="hidden sm:flex sm:flex-1 sm:justify-end">
+          <Link to="login" className="text-sm font-semibold leading-6 text-gray-900">
+            Log in <span aria-hidden="true">&rarr;</span>
+          </Link>
+        </div>
+      </nav>
+
+      <Dialog as="div" className="sm:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
+        <div className="fixed inset-0 z-10" />
+        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+          <div className="flex items-center justify-between">
+            <a href="#" className="-m-1.5 p-1.5">
+              <span className="sr-only">CodeLearn</span>
+              <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?" alt="" />
+            </a>
+            <button
+              type="button"
+              className="-m-2.5 rounded-md p-2.5 text-gray-700"
+              onClick={() => setMobileMenuOpen(false)}
+            >
+              <span className="sr-only">Close menu</span>
+              <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+            </button>
+          </div>
+          <div className="mt-6 flow-root">
+            <div className="-my-6 divide-y divide-gray-500/10">
+              <div className="space-y-2 py-6">
+                <Link
+                  to="testings"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                >
+                  Testings
+                </Link>
+                <Link
+                  to="about"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                >
+                  About
+                </Link>
+              </div>
+              <div className="py-6">
+                <Link
+                  to="login"
+                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                >
+                  Log in
+                </Link>
+              </div>
+            </div>
+          </div>
+        </Dialog.Panel>
+      </Dialog>
+    </header>
+  );
 }
