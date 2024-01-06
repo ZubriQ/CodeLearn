34f81983c0a858a50a9d35111c3fc30d9550fc78
ZubriQ <zubrich157@gmail.com>
2024-01-06 22:22:34 +0700

    feat: jwt token generator interface

---
+++ src/CodeLearn.Application/Common/Interfaces/IJwtTokenGenerator.cs
@@ -0,0 +1,6 @@
+﻿namespace CodeLearn.Application.Common.Interfaces;
+
+public interface IJwtTokenGenerator
+{
+    string GenerateToken(Guid userId, string firstName, string lastName, string? patronymic);
+}
