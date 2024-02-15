122f14e5ddcc07b371697a573fff2f32ae04b8e8
ZubriQ <zubrich157@gmail.com>
2024-02-15 22:13:01 +0630

    feat: axios with student groups request call

---
+++ src/CodeLearn.WebApp/src/api/agent.ts
@@ -0,0 +1,23 @@
+import axios, { AxiosResponse } from 'axios';
+
+axios.defaults.baseURL = 'http://localhost:/5081/api/';
+
+const responseBody = (response: AxiosResponse) => response.data;
+
+const requests = {
+  get: (url: string) => axios.get(url).then(responseBody),
+  post: (url: string, body: {}) => axios.post(url, body).then(responseBody),
+  put: (url: string, body: {}) => axios.put(url, body).then(responseBody),
+  delete: (url: string) => axios.delete(url).then(responseBody),
+};
+
+const StudentGroup = {
+  list: () => requests.get('student-groups'),
+  details: (id: number) => requests.get(`student-groups/${id}`),
+};
+
+const agent = {
+  StudentGroup,
+};
+
+export default agent;
--- src/CodeLearn.WebApp/src/features/dashboard/pages/StudentGroups.page.tsx
+++ src/CodeLearn.WebApp/src/features/dashboard/pages/StudentGroups.page.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { InformationCircleIcon } from '@heroicons/react/16/solid';
 import { useDashboardPageTitle } from '@/components/layout';
 import { StudentGroup } from '@/features/dashboard/models/StudentGroup.ts';
@@ -17,70 +17,18 @@ import { Button } from '@/components/ui/button.tsx';
 import { Label } from '@/components/ui/label.tsx';
 import { Input } from '@/components/ui/input.tsx';
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip.tsx';
-
-const studentGroups: StudentGroup[] = [
-  {
-    id: 1,
-    enrolmentYear: 2020,
-    name: 'IF-60',
-  },
-  {
-    id: 2,
-    enrolmentYear: 2021,
-    name: 'IF-70',
-  },
-  {
-    id: 3,
-    enrolmentYear: 2022,
-    name: 'IF-80',
-  },
-  {
-    id: 4,
-    enrolmentYear: 2023,
-    name: 'IF-90',
-  },
-  {
-    id: 5,
-    enrolmentYear: 2024,
-    name: 'IF-10',
-  },
-  {
-    id: 6,
-    enrolmentYear: 2018,
-    name: 'IF-20',
-  },
-  {
-    id: 7,
-    enrolmentYear: 2015,
-    name: 'IF-30',
-  },
-  {
-    id: 8,
-    enrolmentYear: 2014,
-    name: 'IF-30',
-  },
-  {
-    id: 9,
-    enrolmentYear: 2013,
-    name: 'IF-30',
-  },
-  {
-    id: 10,
-    enrolmentYear: 2012,
-    name: 'IF-30',
-  },
-  {
-    id: 11,
-    enrolmentYear: 2010,
-    name: 'IF-300',
-  },
-];
+import agent from '@/api/agent.ts';
 
 function StudentGroupsPage() {
   const [, setCurrentPageTitle] = useDashboardPageTitle();
+  const [studentGroups, setStudentGroups] = useState<StudentGroup[]>([]);
 
   useEffect(() => {
     setCurrentPageTitle('Student Groups');
+  }, []);
+
+  useEffect(() => {
+    agent.StudentGroup.list().then((groups) => setStudentGroups(groups));
   }, []);
 
   return (
