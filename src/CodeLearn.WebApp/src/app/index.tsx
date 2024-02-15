34400790ee6b1d823fab4728071fb0dd70111705
ZubriQ <zubrich157@gmail.com>
2024-02-16 02:46:09 +0630

    fix: radix ui toast dep, remove toast provider

--- src/CodeLearn.WebApp/src/app/index.tsx
+++ src/CodeLearn.WebApp/src/app/index.tsx
@@ -1,17 +1,16 @@
 import { Outlet, ScrollRestoration } from 'react-router-dom';
 import useDebugRender from 'tilg';
-import { ToastProvider } from '@/components/ui/toast.tsx';
 import { Toaster } from '@/components/ui/toaster.tsx';
 
 function App() {
   useDebugRender();
 
   return (
-    <ToastProvider>
+    <>
       <Outlet />
       <Toaster />
       <ScrollRestoration />
-    </ToastProvider>
+    </>
   );
 }
 
