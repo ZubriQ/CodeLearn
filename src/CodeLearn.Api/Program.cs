a5f61b9d8ca7a3c801e982994f334e66e208bf7e
ZubriQ <zubrich157@gmail.com>
2024-03-15 21:32:41 +0700

    chore: change connection string

--- src/CodeLearn.Api/Program.cs
+++ src/CodeLearn.Api/Program.cs
@@ -12,15 +12,13 @@ var builder = WebApplication.CreateBuilder(args);
     });
 
     builder.Services.AddApplication();
-
     builder.Services.AddInfrastructure(builder.Configuration);
-
     builder.Services.AddWebServices();
 }
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment()) 
+if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
