84eb4fd1565cb1dc4876871366c6259c80dc73f4
ZubriQ <zubrich157@gmail.com>
2024-03-29 19:36:35 +0700

    feat: delete exercise endpoint

---
+++ src/CodeLearn.Api/Controllers/ExercisesController.cs
@@ -0,0 +1,20 @@
+﻿using CodeLearn.Application.Exercises.Commands.DeleteExerecise;
+
+namespace CodeLearn.Api.Controllers;
+
+public sealed class ExercisesController(ISender _sender) : ApiControllerBase
+{
+    [HttpDelete("{exerciseId:int}")]
+    [ProducesResponseType(typeof(Success), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> Delete(int exerciseId)
+    {
+        var result = await _sender.Send(new DeleteExerciseCommand(exerciseId));
+
+        return result.Match(
+            success => Ok(success),
+            _ => Problem(statusCode: StatusCodes.Status404NotFound, title: "Exercise not found."),
+            _ => Problem(statusCode: StatusCodes.Status400BadRequest, title: "Invalid id value."));
+    }
+}
---
+++ src/CodeLearn.Application/Exercises/Commands/DeleteExerecise/DeleteExercise.cs
@@ -0,0 +1,31 @@
+﻿using CodeLearn.Domain.Exercises.ValueObjects;
+
+namespace CodeLearn.Application.Exercises.Commands.DeleteExerecise;
+
+public record DeleteExerciseCommand(int Id) : IRequest<OneOf<Success, NotFound, BadRequest>>;
+
+public class DeleteExerciseCommandHandler(IApplicationDbContext _context)
+    : IRequestHandler<DeleteExerciseCommand, OneOf<Success, NotFound, BadRequest>>
+{
+    public async Task<OneOf<Success, NotFound, BadRequest>> Handle(DeleteExerciseCommand request, CancellationToken cancellationToken)
+    {
+        if (request.Id <= 0)
+        {
+            return new BadRequest();
+        }
+
+        var exercise = await _context.Exercises
+            .FirstOrDefaultAsync(x => x.Id == ExerciseId.Create(request.Id), cancellationToken);
+
+        if (exercise is null)
+        {
+            return new NotFound();
+        }
+
+        _context.Exercises.Remove(exercise);
+
+        await _context.SaveChangesAsync(cancellationToken);
+
+        return new Success();
+    }
+}
