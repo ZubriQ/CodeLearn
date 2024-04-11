d826d316b41a632a8a793f53d4cd7efab290b71b
ZubriQ <zubrich157@gmail.com>
2024-04-11 23:28:53 +0700

    refactor: remove auth mapping config

--- src/CodeLearn.Api/Common/Mapping/UserMappingConfig.cs
+++ src/CodeLearn.Api/Common/Mapping/UserMappingConfig.cs
@@ -1,4 +1,7 @@
-﻿using CodeLearn.Application.Users;
+﻿using CodeLearn.Application.Common.IdentityModels;
+using CodeLearn.Application.Users;
+using CodeLearn.Application.Users.Commands.Login;
+using CodeLearn.Application.Users.Commands.RegisterStudent;
 using CodeLearn.Contracts.Users;
 
 namespace CodeLearn.Api.Common.Mapping;
@@ -9,5 +12,16 @@ public class UserMappingConfig : IRegister
     {
         config.NewConfig<UserDto, StudentResponse>()
             .Map(dest => dest.FullName, src => $"{src.LastName} {src.FirstName} {src.Patronymic}".Trim());
+
+        config.NewConfig<RegisterStudentRequest, RegisterStudentCommand>()
+            .ConstructUsing(src => new RegisterStudentCommand(
+                UserFullName.Create(src.FirstName, src.LastName, src.Patronymic),
+                StudentUserDetails.Create(src.StudentGroupName, src.UserCode)));
+
+        //config.NewConfig<RegisterTeacherRequest, RegisterTeacherCommand>()
+        //    .ConstructUsing(src => new RegisterTeacherCommand(
+        //        UserFullName.Create(src.FirstName, src.LastName, src.Patronymic)));
+
+        config.NewConfig<LoginRequest, LoginCommand>();
     }
 }
